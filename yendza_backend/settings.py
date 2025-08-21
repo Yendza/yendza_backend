3f8dfc8aaa9a349c09cb44f8fb2895955719df6f
Yendza <yendzatech@gmail.com>
2025-08-21 10:43:56 +0200

    Config STATIC_ROOT and WhiteNoise for Render

--- yendza_backend/settings.py
+++ yendza_backend/settings.py
@@ -121,7 +121,6 @@ DATABASES = {
 
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=True, cast=bool)
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1").split(",")
 
 
 # Password validation
